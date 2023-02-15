a9a2cda0fef0797c529bd6ab8003777215aff005
unknown <jemali.ergemlidze.1@btu.edu.ge>
2023-02-15 14:00:55 +0400

    cc

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { Navbar } from "./components/Navbar";
+import { Why } from "./components/Why";
 import './styles/main.scss'
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Why/>
     </>
   );
 }
---
+++ src/components/Why.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Why = () => {
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+                
+            </div>
+            <div className="col-md-6">1</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
